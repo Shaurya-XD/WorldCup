a5a3ee8864d9bed3654aaaf2db2e701021f9507d
Shaurya Kumar <your-github-email@example.com>
2026-06-26 22:12:05 +0530

    completed temas-page

--- 2D_Animation/src/components/teams/TeamCard.jsx
+++ 2D_Animation/src/components/teams/TeamCard.jsx
@@ -1,18 +1,69 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import trophy from '../../assets/trophy.png'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
-const TeamCard = ({image1, image2}) => {
+const TeamCard = ({ image1, image2, name1, name2, win1, win2, awards1, awards2 }) => {
+    const teamInfo = useRef(null)
+    const [name, setName] = useState("")
+    const [win, setWin] = useState(0)
+    const [n, setN] = useState(0)
+
+    const handleMouseEnter = () => {
+        gsap.to(teamInfo.current, {
+            translateY: '0%'
+        })
+    }
+
+    const handleMouseExit = () => {
+        gsap.to(teamInfo.current, {
+            translateY: '-100%'
+        })
+    }
+
     return (
         <div className='w-full h-full mb-[2vh] flex gap-[0.6vw]'>
-            <div className='group relative w-1/2 h-full bg-green-400 hover:rounded-[3vw] overflow-hidden transition-all'>
-                <img className='h-full w-full group-hover:scale-105 transition-all object-cover object-center' src={image1} />
-                <div className='opacity-0 group-hover:opacity-100 transition-all absolute top-0 left-0 bg-black/30 h-full w-full flex items-center justify-center'>
-                    <h2 className='font-[font1] text-[3vw] text-white uppercase border-4 rounded-[3vw] h-[8vh] px-[1vw]'>View Team</h2>
+
+            <div ref={teamInfo} className='fixed w-full h-[10.5vh] top-[0] left-0 z-[55] -translate-y-full'>
+                <div className='h-full w-full'>
+                    <div className='bg-green-100 w-full h-[5.5vh]'></div>
+                    <div className='w-full h-[5vh] border-b-2 border-t-2 bg-green-300 flex justify-between items-center'>
+                        <h2 className='pl-[4.5vw] font-[font2] text-[4vh] uppercase'>{name}</h2>
+                        <div className='flex justify-start -space-x-[9vw]'>
+                            {Array.from({ length: n }).map((_, index) => (
+                                <img
+                                    key={index}
+                                    className="h-[11vh] pr-[7vw] -mt-[4.5vh] object-cover"
+                                    src={trophy}
+                                    alt="Trophy"
+                                />
+                            ))}
+                        </div>
+                        <h2 className='font-[font2] text-[4vh] uppercase'>{win}%</h2>
+                    </div>
                 </div>
             </div>
-            <div className='group relative w-1/2 h-full bg-green-400 hover:rounded-[3vw] overflow-hidden transition-all'>
+
+            <div onMouseEnter={() => {
+                handleMouseEnter();
+                setName(name1)
+                setWin(win1)
+                setN(awards1)
+            }} onMouseLeave={handleMouseExit} className='group relative w-1/2 h-full bg-green-400 hover:rounded-[3vw] overflow-hidden transition-all'>
+                <img className='h-full w-full group-hover:scale-105 transition-all object-cover object-center' src={image1} />
+                <div className='opacity-0 group-hover:opacity-100 transition-all absolute top-0 left-0 bg-black/30 h-full w-full flex items-center justify-center'>
+                    <h2 className='font-[font1] text-[3vw] text-white uppercase border-4 rounded-[3vw] h-[8vh] px-[1vw]'>{name1}</h2>
+                </div>
+            </div>
+            <div onMouseEnter={() => {
+                handleMouseEnter();
+                setName(name2)
+                setWin(win2)
+                setN(awards2)
+            }} onMouseLeave={handleMouseExit} className='group relative w-1/2 h-full bg-green-400 hover:rounded-[3vw] overflow-hidden transition-all'>
                 <img className='h-full w-full group-hover:scale-105 transition-all object-cover object-center' src={image2} />
                 <div className='opacity-0 group-hover:opacity-100 transition-all absolute top-0 left-0 bg-black/30 h-full w-full flex items-center justify-center'>
-                    <h2 className='font-[font1] text-[3vw] text-white uppercase border-4 rounded-[3vw] h-[8vh] px-[1vw]'>View Team</h2>
+                    <h2 className='font-[font1] text-[3vw] text-white uppercase border-4 rounded-[3vw] h-[8vh] px-[1vw]'>{name2}</h2>
                 </div>
             </div>
 
--- 2D_Animation/src/pages/Teams.jsx
+++ 2D_Animation/src/pages/Teams.jsx
@@ -17,10 +17,10 @@ gsap.registerPlugin(ScrollTrigger)
 
 const Teams = () => {
   const sectionRef = useRef(null)
-  const teamInfo = useRef(null)
-  const [name, setName] = useState("")
-  const [win, setWin] = useState(0)
-  const [n, setN] = useState(0)
+  // const teamInfo = useRef(null)
+  // const [name, setName] = useState("")
+  // const [win, setWin] = useState(0)
+  // const [n, setN] = useState(0)
 
   useGSAP(() => {
     gsap.from(
@@ -42,22 +42,22 @@ const Teams = () => {
     ScrollTrigger.refresh();
   }, { scope: sectionRef });
 
-  const handleMouseEnter = () => {
-    gsap.to(teamInfo.current, {
-      translateY: '0%'
-    })
-  }
+  // const handleMouseEnter = () => {
+  //   gsap.to(teamInfo.current, {
+  //     translateY: '0%'
+  //   })
+  // }
 
-  const handleMouseExit = () => {
-    gsap.to(teamInfo.current, {
-      translateY: '-100%'
-    })
-  }
+  // const handleMouseExit = () => {
+  //   gsap.to(teamInfo.current, {
+  //     translateY: '-100%'
+  //   })
+  // }
 
   return (
     <div className='p-4 overflow-hidden'>
 
-      <div ref={teamInfo} className='fixed w-full h-[10.5vh] top-[0] left-0 z-[55] -translate-y-full'>
+      {/* <div ref={teamInfo} className='fixed w-full h-[10.5vh] top-[0] left-0 z-[55] -translate-y-full'>
         <div className='h-full w-full'>
           <div className='bg-green-100 w-full h-[5.5vh]'></div>
           <div className='w-full h-[5vh] border-b-2 border-t-2 bg-green-300 flex justify-between items-center'>
@@ -75,36 +75,16 @@ const Teams = () => {
             <h2 className='font-[font2] text-[4vh] uppercase'>{win}%</h2>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <div className=' pt-[45vh] '>
         <h2 className='font-[font2] text-[10vw] uppercase'>Teams</h2>
       </div>
       <div ref={sectionRef} className='-mt-[8vh] flex flex-col gap-3'>
-        <div className='card h-[60vh]' onMouseEnter={() => {
-          handleMouseEnter();
-          setName('portugal')
-          setWin(60)
-          setN(1)
-        }} onMouseLeave={handleMouseExit}><TeamCard image1={portugal} image2={france} /></div>
-        <div className='card h-[60vh]' onMouseEnter={() => {
-          handleMouseEnter();
-          setName('portugal')
-          setWin(60)
-          setN(1)
-        }} ><TeamCard image1={argentina} image2={england} /></div>
-        <div className='card h-[60vh]' onMouseEnter={() => {
-          handleMouseEnter();
-          setName('portugal')
-          setWin(60)
-          setN(1)
-        }} ><TeamCard image1={brazil} image2={germany} /></div>
-        <div className='card h-[60vh]' onMouseEnter={() => {
-          handleMouseEnter();
-          setName('portugal')
-          setWin(60)
-          setN(1)
-        }} ><TeamCard image1={spain} image2={netherlands} /></div>
+        <div className='card h-[60vh]' ><TeamCard image1={portugal} image2={france} name1={'portugal'} name2={'france'} win1={70} win2={69} awards1={0} awards2={2} /></div>
+        <div className='card h-[60vh]' ><TeamCard image1={argentina} image2={england} name1={'argentina'} name2={'england'} win1={76} win2={65} awards1={3} awards2={1} /></div>
+        <div className='card h-[60vh]' ><TeamCard image1={brazil} image2={germany} name1={'brazil'} name2={'germany'} win1={41} win2={44} awards1={5} awards2={4} /></div>
+        <div className='card h-[60vh]' ><TeamCard image1={spain} image2={netherlands} name1={'spain'} name2={'netherlands'} win1={69} win2={60} awards1={4} awards2={0} /></div>
       </div>
       <div className='w-screen h-[35vh]'></div>
     </div>
